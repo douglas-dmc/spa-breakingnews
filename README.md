747017caaeef004019a7dcecb990e85bf9254d1f
Douglas Marques <douglasmarques37@gmail.com>
2023-08-02 19:57:49 -0300

    montando a estrutura de pastas

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Criação do front-end para o App MERN STACK (spa-breakingnews)
+==============================================
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
@@ -6,3 +8,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Instalação do vite + react
+- `npm create vite@latest spa-breakingnews`
+- `cd spa-breakingnews`
+- `npm install`
+- `npm run dev`
---
+++ src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "Notícia 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis molestiae odio ad est reiciendis perspiciatis, quas eius! Fuga amet unde hic. Unde laboriosam, ex quod iure dolores facilis reprehenderit distinctio praesentium animi placeat aliquid earum eveniet obcaecati quo architecto.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxpZ1CFFHpJXq6PHwPrTqAtK6sRCHM7xGuFA&usqp=CAU",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis molestiae odio ad est reiciendis perspiciatis, quas eius! Fuga amet unde hic. Unde laboriosam, ex quod iure dolores facilis reprehenderit distinctio praesentium animi placeat aliquid earum eveniet obcaecati quo architecto.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxpZ1CFFHpJXq6PHwPrTqAtK6sRCHM7xGuFA&usqp=CAU",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis molestiae odio ad est reiciendis perspiciatis, quas eius! Fuga amet unde hic. Unde laboriosam, ex quod iure dolores facilis reprehenderit distinctio praesentium animi placeat aliquid earum eveniet obcaecati quo architecto.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxpZ1CFFHpJXq6PHwPrTqAtK6sRCHM7xGuFA&usqp=CAU",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 4",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis molestiae odio ad est reiciendis perspiciatis, quas eius! Fuga amet unde hic. Unde laboriosam, ex quod iure dolores facilis reprehenderit distinctio praesentium animi placeat aliquid earum eveniet obcaecati quo architecto.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxpZ1CFFHpJXq6PHwPrTqAtK6sRCHM7xGuFA&usqp=CAU",
+        likes: 2,
+        comments: 4,
+    },
+]
